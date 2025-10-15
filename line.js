340122efcbdefd503406f0cb095dc90e0013afa5
chokhueyyan <140725369+chokhueyyan@users.noreply.github.com>
2025-10-15 22:59:58 +0800

    Add files via upload

---
+++ line.js
@@ -0,0 +1,6 @@
+
+var vg_1 = "line.json";
+vegaEmbed("#line", vg_1).then(function(result) {
+// Access the Vega view instance
+//     (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
